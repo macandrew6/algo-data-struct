3939276c9e0fc05cfdf0ad6fd99b8f143301d4bc
macandrew6 <mr.betterandrewmac@gmail.com>
2020-03-25 20:50:09 -0700

    passed all the test cases for the stack

--- stack_project/lib/stack.js
+++ stack_project/lib/stack.js
@@ -21,11 +21,49 @@
 // -----------
 
 class Node {
-
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
 
+  push(value) {
+    const node = new Node(value);
+    if(!this.top) {
+      this.top = node;
+      this.bottom = node;
+    } else {
+      const temp = this.top;
+      this.top = node;
+      this.top.next = temp;
+    }
+    this.length += 1;
+    return this.length;
+  }
+
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+    if (this.length === 1) {
+      this.bottom = null;
+    }
+    const temp = this.top;
+    this.top = this.top.next;
+    this.length -= 1;
+    return temp.value;
+  }
+
+  size() {
+    return this.length;
+  }
 }
 
 exports.Node = Node;
