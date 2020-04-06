16053c3a8ee4d96ad2da27067b8ca4d345a7fec2
macandrew6 <mr.betterandrewmac@gmail.com>
2020-04-05 17:53:32 -0700

    starting search Recur

--- bst_project/lib/bst.js
+++ bst_project/lib/bst.js
@@ -10,6 +10,35 @@ class BST {
   constructor() {
     this.root = null;
   }
+
+  insert(val, root = this.root) {
+    if (!this.root) {
+      this.root = new TreeNode(val);
+      return;
+    }
+
+    if (val < root.val) {
+      if (!root.left) {
+        root.left = new TreeNode(val);
+      } else {
+        this.insert(val, root.left);
+      }
+    } else {
+      if (!root.right) {
+        root.right = new TreeNode(val);
+      } else {
+        this.insert(val, root.right);
+      }
+    }
+  }
+
+  searchRecur(value) {
+    if (!this.root) {
+      return false;
+    }
+  }
+
+  searchIter(value) {}
 }
 
 module.exports = {
