abcd612f348f297d53aeeb20a33b5019baaaec55
macandrew6 <mr.betterandrewmac@gmail.com>
2020-04-07 16:21:08 -0700

    pseudo code is balanced

--- bst_project/lib/leet_code_110.js
+++ bst_project/lib/leet_code_110.js
@@ -1,7 +1,6 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/balanced-binary-tree/
 
-
 function isBalanced(root) {
-
+  // iterate through the tree and return true or false if the tree is balanced
 }
