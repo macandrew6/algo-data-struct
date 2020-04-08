999311e23f3e9238770157de8106ec9322f3a10d
macandrew6 <mr.betterandrewmac@gmail.com>
2020-04-07 19:13:15 -0700

    creates a helper function to check the height of the root and its left and right properties

--- bst_project/lib/leet_code_110.js
+++ bst_project/lib/leet_code_110.js
@@ -1,6 +1,11 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/balanced-binary-tree/
 
+const getHeight = root => {
+  if (!root) return -1;
+  return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+};
+
 function isBalanced(root) {
   // iterate through the tree and return true or false if the tree is balanced
 }
