c222d98259c142fb6fca6a49c91f248a37d45312
macandrew6 <mr.betterandrewmac@gmail.com>
2020-04-09 19:01:01 -0700

    starts sortedArray to BST

--- bst_project/lib/leet_code_108.js
+++ bst_project/lib/leet_code_108.js
@@ -1,7 +1,8 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 
-
 function sortedArrayToBST(nums) {
-
+  if (nums.length === 0) {
+    return null;
+  }
 }
