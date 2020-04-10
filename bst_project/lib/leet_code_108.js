cf44f80d8863e8bd31fadf2e274069ff6c75ab62
macandrew6 <mr.betterandrewmac@gmail.com>
2020-04-09 19:02:43 -0700

    pseudo codes

--- bst_project/lib/leet_code_108.js
+++ bst_project/lib/leet_code_108.js
@@ -2,6 +2,8 @@
 //  https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 
 function sortedArrayToBST(nums) {
+  // we know nums is a sorted array
+  // if there are no numbers in the array return null;
   if (nums.length === 0) {
     return null;
   }
