412681330f77948396ccbaee096191122f467c2d
macandrew6 <mr.betterandrewmac@gmail.com>
2020-04-09 19:07:37 -0700

    defines a tree node class

--- bst_project/lib/leet_code_108.js
+++ bst_project/lib/leet_code_108.js
@@ -1,10 +1,20 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = this.right = null;
+  }
+}
+
 function sortedArrayToBST(nums) {
   // we know nums is a sorted array
   // if there are no numbers in the array return null;
   if (nums.length === 0) {
     return null;
   }
+
+  let midIdx = Math.floor(nums.length / 2);
+  let root = new TreeNode(nums[midIdx]);
 }
