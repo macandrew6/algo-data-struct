82e740966bb90e154fe650c59c179982562ff2bd
macandrew6 <mr.betterandrewmac@gmail.com>
2020-04-14 21:25:38 -0700

    de constructs array all the way down

--- tree_order_project/lib/tree_order.js
+++ tree_order_project/lib/tree_order.js
@@ -1,11 +1,16 @@
 function inOrderArray(root) {
   if (!root) return [];
+  // deconstructs the return value of inOrderArray(root) =>
+  // which is an array
+
   return [...inOrderArray(root.left), root.val, ...inOrderArray(root.right)];
 }
 
 function postOrderArray(root) {
   if (!root) return [];
-  // deconstructs the return value of postOrderArray
+  // deconstructs the return value of postOrderArray(root) =>
+  // which is an array
+
   return [
     ...postOrderArray(root.left),
     ...postOrderArray(root.right),
