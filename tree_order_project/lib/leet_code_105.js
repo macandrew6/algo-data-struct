cd7a07dbb19eae1517e41cda04fd6a59d3e713d8
macandrew6 <mr.betterandrewmac@gmail.com>
2020-04-15 18:39:58 -0700

    pseudo codes build tree

--- tree_order_project/lib/leet_code_105.js
+++ tree_order_project/lib/leet_code_105.js
@@ -1,9 +1,9 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
 
-const { TreeNode } = require('./tree_node.js');
-
+const { TreeNode } = require("./tree_node.js");
 
 function buildTree(preorder, inorder) {
-
+  // if the tree is empty return null
+  if (!preorder.length && !inorder.length) return null;
 }
