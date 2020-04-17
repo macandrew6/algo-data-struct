c3db515805202caaeda670a8b51fdcef1e0e9e03
macandrew6 <mr.betterandrewmac@gmail.com>
2020-04-16 17:32:35 -0700

    seperates left and right inorder

--- tree_order_project/lib/leet_code_105.js
+++ tree_order_project/lib/leet_code_105.js
@@ -6,6 +6,12 @@ const { TreeNode } = require("./tree_node.js");
 function buildTree(preorder, inorder) {
   // if the tree is empty return null
   if (!preorder.length && !inorder.length) return null;
+  let rootVal = preorder[0];
+
+  let root = new TreeNode(rootVal);
+  let midIdx = inorder.indexOf(rootVal);
+  let leftInorder = inorder.slice(0, midIdx);
+  let rightInorder = inorder.slice(midIdx + 1);
 }
 
 console.log(buildTree([], []));
