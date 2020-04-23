f73c80d6e43e23f367872f2c5f165c9c6e419299
macandrew6 <mr.betterandrewmac@gmail.com>
2020-04-22 17:31:11 -0700

    diagrams dfs

--- depth_breadth_first_project/lib/depth_first_search.js
+++ depth_breadth_first_project/lib/depth_first_search.js
@@ -1,8 +1,19 @@
 function depthFirstSearch(root, targetVal) {
   // pseudo code depthFirstSearch
   // use a stack
+  // whenever popping a node from the stack consider it visited
+  const stack = [];
 }
 
 module.exports = {
   depthFirstSearch
 };
+
+/*
+DFS
+        A
+      /   \
+    B       E
+  /   \       \
+C       D       F  
+*/
