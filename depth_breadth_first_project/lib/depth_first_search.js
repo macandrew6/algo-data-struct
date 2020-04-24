3c4d3577a96c4e4adc6a4b541037ad6c2acb51d4
macandrew6 <mr.betterandrewmac@gmail.com>
2020-04-24 12:27:16 -0700

    init tree nodes

--- depth_breadth_first_project/lib/depth_first_search.js
+++ depth_breadth_first_project/lib/depth_first_search.js
@@ -18,3 +18,20 @@ DFS
   /   \       \
 C       D       F  
 */
+
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+let a = new TreeNode("a");
+let b = new TreeNode("b");
+let c = new TreeNode("c");
+let d = new TreeNode("d");
+let e = new TreeNode("e");
+let f = new TreeNode("f");
+let g = new TreeNode("g");
+let h = new TreeNode("h");
