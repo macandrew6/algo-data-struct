d429aa6cf7a90cee97e53e427754fe4698bb86a4
macandrew6 <mr.betterandrewmac@gmail.com>
2020-04-24 02:21:07 -0700

    implement stack in dfs

--- depth_breadth_first_project/lib/depth_first_search.js
+++ depth_breadth_first_project/lib/depth_first_search.js
@@ -1,6 +1,7 @@
 function depthFirstSearch(root, targetVal) {
   // pseudo code depthFirstSearch
   // use a stack
+  // push a node on to the stack at every leaf level
   // whenever popping a node from the stack consider it visited
   const stack = [];
 }
