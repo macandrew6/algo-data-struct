16c654789fecf4712e88585c2b2a49b6fdaa2348
macandrew6 <mr.betterandrewmac@gmail.com>
2020-04-28 22:27:32 -0700

    notes

--- depth_breadth_first_project/lib/breadth_first_array.js
+++ depth_breadth_first_project/lib/breadth_first_array.js
@@ -1,5 +1,19 @@
 function breadthFirstArray(root) {
   // breadth first search uses a queue
+  // save visited node values in an array
+  let visited = [];
+  let queue = [root];
+
+  while (queue.length) {
+    let node = queue.shift();
+
+    visited.push(node.val);
+
+    if (node.left) queue.push(node.left);
+    if (node.right) queue.push(node.right);
+  }
+
+  return visited;
 }
 
 module.exports = {
