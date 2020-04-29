b0117b7878e5b4dcefbadb341fcd7a49b755896a
macandrew6 <mr.betterandrewmac@gmail.com>
2020-04-29 15:39:52 -0700

    pseudo codes bfs

--- depth_breadth_first_project/lib/breadth_first_array.js
+++ depth_breadth_first_project/lib/breadth_first_array.js
@@ -5,8 +5,10 @@ function breadthFirstArray(root) {
   let queue = [root];
 
   while (queue.length) {
+    // remove a node off the front of the queue when visited
     let node = queue.shift();
 
+    // build our array one node value at a time
     visited.push(node.val);
 
     if (node.left) queue.push(node.left);
