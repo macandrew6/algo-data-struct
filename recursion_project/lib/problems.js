bda20d669a8ddd54468b86a1ffb0e98686b27f9c
macandrew6 <mr.betterandrewmac@gmail.com>
2020-05-05 18:05:31 -0700

    reverses a string recursively

--- recursion_project/lib/problems.js
+++ recursion_project/lib/problems.js
@@ -49,7 +49,10 @@ function sumArray(array) {
 // reverseString("c")           // => "c"
 // reverseString("internet")    // => "tenretni"
 // reverseString("friends")     // => "sdneirf"
-function reverseString(str) {}
+function reverseString(str) {
+  if (!str.length) return "";
+  return reverseString(str.slice(1)) + str[0];
+}
 
 // Write a function, pow(base, exponent), that takes in two numbers.
 // The function should calculate the base raised to the exponent power.
