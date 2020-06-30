a00e2aae7e0f71cce7cb6a4c699fbad80d752853
macandrew6 <mr.betterandrewmac@gmail.com>
2020-06-30 16:01:16 -0700

    adds pseudo code

--- memoization_project/lib/leet_code_518.js
+++ memoization_project/lib/leet_code_518.js
@@ -30,7 +30,6 @@ var change = function (amt, coins, memo = {}) {
   // counter
   // increment that counter until the amount is met
   // return 1 + the call to the change fn
-  // returns each time it reaches the stack
   if (amt === 0) return 1;
   if (coins.length === 0) return 0;
 
