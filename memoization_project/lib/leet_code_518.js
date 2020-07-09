a6a79ec7e58f288a4a44dddb7fc65c69c924b352
macandrew6 <mr.betterandrewmac@gmail.com>
2020-07-08 17:25:08 -0700

    testing

--- memoization_project/lib/leet_code_518.js
+++ memoization_project/lib/leet_code_518.js
@@ -26,6 +26,7 @@ Explanation: the amount of 3 cannot be made up just with coins of
 var change = function (amt, coins, memo = {}) {
   // pseudo memoization
   // testing
+  // adding tests
   if (amt === 0) return 1;
   if (coins.length === 0) return 0;
 
