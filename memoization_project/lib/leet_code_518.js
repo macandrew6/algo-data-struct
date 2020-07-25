2d5caf64a74725877ba5148b3328c4a9455d4a88
macandrew6 <mr.betterandrewmac@gmail.com>
2020-07-24 20:13:59 -0700

    fix another issue

--- memoization_project/lib/leet_code_518.js
+++ memoization_project/lib/leet_code_518.js
@@ -27,6 +27,7 @@ var change = function (amt, coins, memo = {}) {
   // pseudo memoization
   // testing
   // moving testing suite to another file
+  // fixing one more issue
 
   if (amt === 0) return 1;
   if (coins.length === 0) return 0;
